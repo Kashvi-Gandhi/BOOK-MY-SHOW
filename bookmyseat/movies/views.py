5a894318440ff8d9f37fcb1addae1b199cd4970f
Kashvi-Gandhi <kashvig2014@gmail.com>
2026-03-08 19:21:12 +0530

    Initial commit

---
+++ bookmyseat/movies/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Movie, Theater, Seat, Booking
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+
+def movie_list(request):
+    search_query = request.GET.get('search')
+    if search_query:
+        movies = Movie.objects.filter(name__icontains=search_query)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'movies/movie_list.html', {'movies': movies})
+
+def theater_list(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    theater = Theater.objects.filter(Movie=movie)
+    return render(request, 'movies/theater_list.html', {'movie': movie, 'theaters': theater})
+
+@login_required(login_url='/login/')
+def book_seats(request, theater_id):
+    theater = get_object_or_404(Theater, id=theater_id)
+    seats = Seat.objects.filter(Theater=theater)
+    if request.method == 'POST':
+        selected_seats = request.POST.getlist('seats')
+        error_seats = []
+        if not selected_seats:
+            return render(request, 'movies/seat_selection.html', {'theater': theater, 'seats': seats, 'error': 'Please select at least one seat.'})
+        for seat_id in selected_seats:
+            seat = get_object_or_404(Seat, id=seat_id, Theater=theater)
+            if seat.is_booked:
+                error_seats.append(seat.seat_number)
+                continue
+            try:
+                Booking.objects.create(user=request.user, seat=seat, movie=theater.Movie, theater=theater)
+                seat.is_booked = True
+                seat.save()
+            except IntegrityError:
+                error_seats.append(seat.seat_number)
+        if error_seats:
+            error_message = f"The following seats are already booked: {', '.join(error_seats)}. Please select different seats."
+            return render(request, 'movies/seat_selection.html', {'theater': theater, 'seats': seats, 'error': error_message})
+        return redirect('profile')
+    return render(request, 'movies/seat_selection.html', {'theater': theater, 'seats': seats})
---
+++ bookmyseat/users/views.py
@@ -0,0 +1,68 @@
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from .forms import UserRegistrationForm, UserUpdateForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from movies.models import Movie, Theater, Seat, Booking
+
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, 'users/home.html', {'movies': movies})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registration successful. You are now logged in.')
+            return redirect('profile')
+        messages.error(request, 'Registration failed. Fix the errors below.')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, 'Logged in successfully.')
+            return redirect(request.POST.get('next') or 'profile')
+        messages.error(request, 'Login failed. Check your credentials and try again.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form, 'next': request.GET.get('next', '')})
+
+@login_required
+def profile(request):
+    booking = Booking.objects.filter(user=request.user)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            if u_form.is_valid():
+                u_form.save()
+                return redirect('profile')
+        else:
+            u_form = UserUpdateForm(instance=request.user)
+        return render(request, 'users/profile.html', {'u_form': u_form, 'preview_mode': False, 'booking': booking})
+
+    # Temporary preview for anonymous users.
+    u_form = UserUpdateForm()
+    return render(request, 'users/profile.html', {'u_form': u_form, 'preview_mode': True})
+
+@login_required
+def reset_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(user=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = PasswordChangeForm(user=request.user)
+    return render(request, 'users/reset_password.html', {'forms': form})
